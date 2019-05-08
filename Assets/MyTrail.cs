7a2d3f9f33d52d3b3d3440fa30802d51fd2450f4
hina3ta <madomagi1013@gmail.com>
2019-05-08 23:52:45 +0900

    クーロン力の動作確認。およびigonoreの設定

---
+++ Assets/MyTrail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTrail : MonoBehaviour {
+    GameObject[] go_list;
+    void Start() {
+        go_list = GameObject.FindGameObjectsWithTag("Player");
+        transform.position = Random.onUnitSphere * 40f;
+        GetComponent<Rigidbody>().AddForce(Vector3.Cross(transform.position, Random.onUnitSphere), ForceMode.Impulse);
+    }
+
+    void FixedUpdate() {
+        foreach (var go in go_list) {
+            if (go.GetInstanceID() != gameObject.GetInstanceID()) {
+                var diff = go.transform.position - transform.position;
+                var len2 = diff.sqrMagnitude;
+                var force = diff.normalized / len2;
+                GetComponent<Rigidbody>().AddForce(force * 1000f);
+            }
+        }
+        GetComponent<Rigidbody>().AddForce(-transform.position * 0.1f);
+    }
+}
